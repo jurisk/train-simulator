8bdc5c13e71e586d6838e022d7e0af848eb69f95
Juris <jurisk@users.noreply.github.com>
2024-05-19 19:38:34 +0300

    Added test coordinate axis.

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
+        .add_systems(Update, draw_test_axis)
         .run();
 }
 
@@ -40,4 +41,11 @@ fn setup(
         transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
+}
+
+fn draw_test_axis(mut gizmos: Gizmos) {
+    let length = 2.0;
+    gizmos.arrow(Vec3::ZERO, Vec3::new(length, 0.0, 0.0), Color::RED);
+    gizmos.arrow(Vec3::ZERO, Vec3::new(0.0, length, 0.0), Color::GREEN);
+    gizmos.arrow(Vec3::ZERO, Vec3::new(0.0, 0.0, length), Color::BLUE);
 }
