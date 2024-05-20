881ab6714d8aacbeae8a6764b3b195627b294743
Juris <jurisk@users.noreply.github.com>
2024-05-20 21:35:38 +0300

    Moved debug gizmos to separate module.

---
+++ src/debug/mod.rs
@@ -0,0 +1,18 @@
+use bevy::app::{App, Update};
+use bevy::math::Vec3;
+use bevy::prelude::{Color, Gizmos, Plugin};
+
+pub struct DebugPlugin;
+
+impl Plugin for DebugPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, draw_test_axis);
+    }
+}
+
+fn draw_test_axis(mut gizmos: Gizmos) {
+    let length = 2.0;
+    gizmos.arrow(Vec3::ZERO, Vec3::new(length, 0.0, 0.0), Color::RED);
+    gizmos.arrow(Vec3::ZERO, Vec3::new(0.0, length, 0.0), Color::GREEN);
+    gizmos.arrow(Vec3::ZERO, Vec3::new(0.0, 0.0, length), Color::BLUE);
+}
