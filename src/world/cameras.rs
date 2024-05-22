d6232fda9f1a7464ba3f35122390047ff76166c6
Juris <jurisk@users.noreply.github.com>
2024-05-22 22:23:12 +0300

    Movable camera

--- src/world/cameras.rs
+++ src/world/cameras.rs
@@ -1,14 +1,21 @@
 use bevy::app::App;
 use bevy::math::Vec3;
-use bevy::prelude::{default, Camera3dBundle, Commands, Plugin, Startup, Transform};
+use bevy::prelude::{
+    default, ButtonInput, Camera3dBundle, Commands, Component, KeyCode, Plugin, Query, Res,
+    Startup, Time, Transform, Update,
+};
 
 pub(crate) struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, create_cameras);
+        app.add_systems(Startup, create_cameras)
+            .add_systems(Update, move_cameras);
     }
 }
+
+#[derive(Component, Default)]
+struct ControllableCamera {}
 
 fn create_cameras(mut commands: Commands) {
     let n = 8.0;
@@ -16,8 +23,39 @@ fn create_cameras(mut commands: Commands) {
     let target = Vec3::ZERO;
     let up = Vec3::Y;
 
-    commands.spawn(Camera3dBundle {
-        transform: from.looking_at(target, up),
-        ..default()
-    });
+    commands.spawn((
+        Camera3dBundle {
+            transform: from.looking_at(target, up),
+            ..default()
+        },
+        ControllableCamera::default(),
+    ));
+}
+
+fn move_cameras(
+    time: Res<Time>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut query: Query<(&ControllableCamera, &mut Transform)>,
+) {
+    for (_, mut transform) in query.iter_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::KeyF) {
+            direction.z -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyS) {
+            direction.z += 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyE) {
+            direction.x -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyD) {
+            direction.x += 1.0;
+        }
+
+        if direction != Vec3::ZERO {
+            direction = direction.normalize();
+            transform.translation += direction * time.delta_seconds();
+        }
+    }
 }
