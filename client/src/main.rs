54f07cbc137ef269aaea5d7caf6888f403cc122c
Juris <jurisk@users.noreply.github.com>
2024-06-01 08:15:24 +0300

    Moving to asset preprocessing

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,6 @@
-use bevy::asset::AssetMetaCheck;
-use bevy::prelude::App;
+use bevy::asset::AssetPlugin;
 use bevy::prelude::PluginGroup;
+use bevy::prelude::{App, AssetMode};
 use bevy::utils::default;
 use bevy::window::{Window, WindowPlugin, WindowResolution};
 use bevy::DefaultPlugins;
@@ -24,14 +24,22 @@ mod states;
 fn main() {
     let mut app = App::new();
     app.init_state::<GameState>();
-    app.add_plugins(DefaultPlugins.build().set(WindowPlugin {
-        primary_window: Some(Window {
-            #[allow(clippy::cast_precision_loss)]
-            resolution: WindowResolution::new(WINDOW_WIDTH as f32, WINDOW_HEIGHT as f32),
-            ..default()
-        }),
-        ..default()
-    }));
+    app.add_plugins(
+        DefaultPlugins
+            .build()
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    #[allow(clippy::cast_precision_loss)]
+                    resolution: WindowResolution::new(WINDOW_WIDTH as f32, WINDOW_HEIGHT as f32),
+                    ..default()
+                }),
+                ..default()
+            })
+            .set(AssetPlugin {
+                mode: AssetMode::Processed,
+                ..default()
+            }),
+    );
     app.add_plugins((
         CommunicationPlugin,
         LightsPlugin,
@@ -39,6 +47,5 @@ fn main() {
         CameraPlugin,
         DebugPlugin,
     ));
-    app.insert_resource(AssetMetaCheck::Never); // Otherwise we were getting 404-s in WASM for `*.wgsl.meta` files
     app.run();
 }
