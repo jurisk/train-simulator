e75a50b2ff9bf5b992ba6016118e466748abc9a5
Juris <jurisk@users.noreply.github.com>
2024-06-12 21:42:23 +0300

    Seems to work locally

--- networking/client/src/bin/client_graphical.rs
+++ networking/client/src/bin/client_graphical.rs
@@ -14,10 +14,11 @@ fn ws_url_from_window_location() -> Option<String> {
     let ws_url = origin_url.clone();
     if origin_url.protocol().starts_with(&"https") { // It can be "https:"...
         ws_url.set_protocol("wss");
+        // If it's `https` then we assume ingress is doing SSL termination and it stays on the same port
     } else {
         ws_url.set_protocol("ws");
+        ws_url.set_port(&format!("{WEBSOCKETS_PORT}"));
     }
-    ws_url.set_port(&format!("{WEBSOCKETS_PORT}"));
     ws_url.set_pathname("/ws");
 
     Some(ws_url.to_string().into())
