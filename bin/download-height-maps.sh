626c4ab56371c51229b674c43fee55f3b1b7e189
Juris <jurisk@users.noreply.github.com>
2024-09-22 07:33:39 +0300

    Minor fixes

--- bin/download-height-maps.sh
+++ bin/download-height-maps.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
-wget https://storage.googleapis.com/ts-height-maps/usa_east/usa_east_etopo_2022.tiff -O misc/assets-original/height-maps/usa_east/usa_east_etopo_2022.tiff
+wget https://storage.googleapis.com/ts-height-maps/usa/usa_east_etopo_2022.tiff -O misc/assets-original/height-maps/usa/usa_east_etopo_2022.tiff
 wget https://storage.googleapis.com/ts-height-maps/europe/europe_etopo_2022.tiff -O misc/assets-original/height-maps/europe/europe_etopo_2022.tiff
